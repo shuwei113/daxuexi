83f80f9632a082f07ec13abb1769404ede081107
shuwei113 <65400351+shuwei113@users.noreply.github.com>
2020-11-24 17:41:06 +0800

    Add files via upload

--- 青年大学习_进阶版.py
+++ 青年大学习_进阶版.py
@@ -10,20 +10,20 @@ requests.urllib3.disable_warnings()
 
 
 # 获取最新一期链接
-def get_newest_url():
+def get_newest_url(order):
     url = 'http://news.cyol.com/node_67071.htm'
     response = requests.get(url, verify=False)
     html = etree.HTML(response.text)
     newest_url = html.xpath(
-        '/html/body/div[@class="mianbody"]/dl[@class="listMM"]/dd[@class="picB"]/ul[@class="movie-list"]/li[1]/a/@href')[
+        '/html/body/div[@class="mianbody"]/dl[@class="listMM"]/dd[@class="picB"]/ul[@class="movie-list"]/li['+str(order)+']/a/@href')[
         0]
 
     return newest_url
 
 
 # 获取最新一期标题
-def get_title_text():
-    url = get_newest_url()
+def get_title_text(order):
+    url = get_newest_url(order)
     response = requests.get(url, verify=False)
     response.encoding = 'utf-8'
     html = etree.HTML(response.text)
@@ -33,8 +33,8 @@ def get_title_text():
 
 
 # 获取完成图片
-def get_endPic():
-    url = get_newest_url()
+def get_endPic(order):
+    url = get_newest_url(order)
     img_url = url.replace('m.html', 'images/end.jpg').replace('index.html', 'images/end.jpg')
     try:
         os.mkdir('images', 777)
@@ -47,8 +47,8 @@ def get_endPic():
 
 
 # 生成HTML文件
-def html_gener():
-    title = get_title_text()
+def html_gener(order):
+    title = get_title_text(order)
 
     html = """
     <html>
@@ -58,7 +58,7 @@ def html_gener():
         <title>{}</title>
     </head>
     <body style="margin: 0;">
-        <img style='width: 100%;height: 100%;' src='end.jpg'>
+        <img style='width: 100%;height: 100%;' src='images/end.jpg'>
     </body>
     </html>
     """.format(title)
@@ -69,8 +69,9 @@ def html_gener():
 
 
 if __name__ == "__main__":
-    print('[*]成功获取新一期：' + get_title_text())
-    get_endPic()
+    order=input('请输入要生成界面为倒数第几期：')
+    print('[*]成功获取：' + get_title_text(order))
+    get_endPic(order)
     print('[*]成功下载图片')
-    html_gener()
+    html_gener(order)
     print('[*]成功生成网页')
